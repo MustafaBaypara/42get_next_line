7ac2f010332a22e84cbe60e52e6383077efbaaf5
Mustafa Baypara <94145699+MustafaBaypara@users.noreply.github.com>
2024-01-19 16:33:17 +0300

    Update get_next_line_bonus.c

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 static char	*copy_stash(char *stash, char *buffer)
 {
